cf53d404ee4e0df478ce4dd3cc7ed991b6e96ea3
Marcello Victor <6786382+narcello@users.noreply.github.com>
2023-08-18 18:08:07 +0000

    fix: throw and error on fetch person to catch it and re open the sign in modal

--- src/hooks/usePerson.ts
+++ src/hooks/usePerson.ts
@@ -7,12 +7,12 @@ function usePerson() {
 
   const fetchPerson = async (name: string) => {
     const result = await getPersonByName("mambu", name);
+    if (!result || result.length === 0) throw Error;
     if (result && result.length) {
       const personProfile = result.filter((item) => item.name === name);
       setPerson(personProfile[0]);
       Promise.resolve();
     }
-    Promise.reject();
   };
 
   return { person, fetchPerson };
